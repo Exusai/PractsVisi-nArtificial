b041377ddb2d8f53d2e083188af54bb3f2bc7466
Faber <a.a.r.samuel99@gmail.com>
2022-03-25 09:31:32 -0600

    added recorte de imgs

---
+++ PractsPy/recorte.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+color = (0, 0, 255)
+grosor = 4
+anchoMin = 100
+
+img = cv.imread("lennaRGB.png", 1)
+imgCopy = img.copy()
+
+cv.imshow("Cuadro", img)
+
+def Region(event, x, y, flags, param):
+    global x1, y1, img
+    if event == cv.EVENT_LBUTTONDOWN:
+        x1, y1 = x,y
+
+    if event == cv.EVENT_MOUSEMOVE and flags == cv.EVENT_FLAG_LBUTTON:
+        img = imgCopy.copy()
+        cv.rectangle(img, (x1, y1), (x, y), color, grosor)
+
+    elif event == cv.EVENT_LBUTTONUP:
+        imgRecorte = imgCopy[y1:y, x1:x]
+        cv.imshow("Recorte", imgRecorte)
+    
+    cv.imshow("Cuadro", img)
+
+cv.setMouseCallback("Cuadro", Region)
+cv.waitKey(0)
