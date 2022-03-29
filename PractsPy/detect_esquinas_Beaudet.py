9e3882be12b7759bb2d86ca8ad6421bdb8161332
Faber <a.a.r.samuel99@gmail.com>
2022-03-28 20:26:09 -0600

    updated beaudet corner detector

--- PractsPy/detect_esquinas_Beaudet.py
+++ PractsPy/detect_esquinas_Beaudet.py
@@ -4,10 +4,15 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 # Load the image in black and white
-I = cv.imread("imgs/edif1.jpg", 0)
+I = cv.imread("imgs/edif1.jpg")
 
 # Add blur to the image
-I = cv.GaussianBlur(I, (5, 5), 0)
+#I = cv.GaussianBlur(I, (5, 5), 0)
+
+Iorg = I.copy()
+
+# Convert to gray scale
+I = cv.cvtColor(I, cv.COLOR_BGR2GRAY)
 
 # x derivative
 sobelx = np.array([[-1, 0, 1],
@@ -19,11 +24,8 @@ sobely = np.array([[-1, -2, -1],
                     [ 0,  0,  0],
                     [ 1,  2,  1]])
 
-# To get second derivative differentiate twice.
 #Ixx = signal.convolve2d(signal.convolve2d(I, sobelx, "same"),sobelx,"same")     
-# Iyy  
 #Iyy = signal.convolve2d(signal.convolve2d(I, sobely, "same"),sobely,"same")
-# Ixy Image 
 #Ixy = signal.convolve2d(signal.convolve2d(I, sobelx, "same"),sobely,"same")
 
 Ix = np.diff(I, axis=0, prepend=0)
@@ -35,34 +37,32 @@ Iyy = np.diff(Iy, axis=1, prepend=0)
 Ixy = np.diff(Ix, axis=1, prepend=0)
 
 """ plt.imshow(Ixx, cmap='gray')
+plt.show()
+
+plt.imshow(Iyy, cmap='gray')
+plt.show()
+
+plt.imshow(Ixy, cmap='gray')
 plt.show() """
 
-""" plt.imshow(Iyy, cmap='gray')
-plt.show() """
-
-""" plt.imshow(Ixy, cmap='gray')
-plt.show() """
-
-# Get Determinant and trace 
+# Operator
 det = Ixx*Iyy - Ixy**2
-trace = Ixx + Iyy
 
-# Harris is det(H) - a * trace(H) let a = 0.2 
-H = det - 0.2 * trace
-det = H
+# get local maxima of det matrix in a 10x10 neighborhood
+#det_max = signal.convolve2d(det, np.ones((10, 10)), "same")
 
-#det = (det)/(1+signal.convolve2d(I, sobelx, "same")**2+signal.convolve2d(I, sobely, "same")**2)**2
-#print(kmin.shape)
+""" plt.imshow(det_max)
+plt.show() """
 
 # binarize the matrix det
-tr = np.average(det) + np.std(det)*2
+tr = np.average(det) + 2*np.std(det)
 #tr = .1*np.max(det)
 
 bin = np.where(det > tr, 255, 0)
 
-ellipticPoints = np.where(det > 0, 255, 0)
+""" ellipticPoints = np.where(det > 0, 255, 0)
 hyperbolicPoints = np.where(det < 0, 255, 0)
-parabolicPoints = np.where(det == 0, 255, 0)
+parabolicPoints = np.where(det == 0, 255, 0) """
 
 """ plt.imshow(hyperbolicPoints)
 plt.show() """
@@ -72,8 +72,8 @@ points = np.where(bin > 0)
 
 #iterate over the list of points and get the coordinates of the points
 for i in range(len(points[0])):
-    cv.circle(I, (points[1][i], points[0][i]), 1, (0, 0, 255), -1)
+    cv.circle(Iorg, (points[1][i], points[0][i]), 1, (0, 0, 255), -1)
 
 # display the image
-cv.imshow("Img", I)
+cv.imshow("Img", Iorg)
 cv.waitKey(0)
